af82a88baf793bb035f486b5b4dbfd421799cdac
Danielle Katz <daniellekatz93@gmail.com>
2016-12-15 14:27:14 -0600

    external navbar

--- browser/react/components/Navbar.js
+++ browser/react/components/Navbar.js
@@ -1,51 +1,52 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router';
 
 class Navbar extends React.Component {
   constructor(props) {
     super(props);
-    // this.renderLoginSignup = this.renderLoginSignup.bind(this);
-    // this.renderLogout = this.renderLogout.bind(this);
+    this.renderLoginSignup = this.renderLoginSignup.bind(this);
+    this.renderLogout = this.renderLogout.bind(this);
   }
 
-  // renderLoginSignup() {
-  //   return (
-  //     <ul className="nav navbar-nav navbar-right">
-  //       <li>
-  //        <Link to="/signup" activeClassName="active">Signup</Link>
-  //       </li>
-  //       <li>
-  //         <Link to="/login" activeClassName="active">Login</Link>
-  //       </li>
-  //     </ul>
-  //   );
-  // }
+  renderLoginSignup() {
+    return (
+      <ul className="nav navbar-nav navbar-right">
+        <li>
+         <Link to="/signup" activeClassName="active">Signup</Link>
+        </li>
+        <li>
+          <Link to="/login" activeClassName="active">Login</Link>
+        </li>
+      </ul>
+    );
+  }
 
-  // renderLogout() {
-  //   return (
-  //     <ul className="nav navbar-nav navbar-right">
-  //       <li>
-  //       <button className="navbar-btn btn btn-default"
-  //         onClick={this.props.logout}>Logout
-  //       </button>
-  //       </li>
-  //     </ul>
-  //   );
-  // }
+  renderLogout() {
+    return (
+      <ul className="nav navbar-nav navbar-right">
+        <li>
+        <button className="navbar-btn btn btn-default"
+          onClick={this.props.logout}>Logout
+        </button>
+        </li>
+      </ul>
+    );
+  }
 
   render() {
     return (
-      <nav class="navbar navbar-inverse">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
+      <nav className="navbar navbar-inverse">
+        <div className="navbar-header">
+          <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
           </button>
-          <Link class="navbar-brand clearfix" to="/"><span class="glyphicon glyphicon-forward"></span><span> S K I P </span></Link>
+          <Link className="navbar-brand clearfix" to="/"><span className="glyphicon glyphicon-forward"></span><span> S K I P </span></Link>
         </div>
-        <div class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
+        <div className="collapse navbar-collapse">
+          <ul className="nav navbar-nav">
             <li>
               <Link to="/bars" activeClassName="active">Home</Link>
             </li>
--- seed.js
+++ seed.js
@@ -8,7 +8,7 @@ var data = {
   bar: [
     {name: "Stout Barrel House", place: {address: "642 N Clark St", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.893710, -87.631468]}, rating: 4, imgURL: "http://www.stoutchicago.com/wp-content/themes/theme-stout/images/logo.png"},
     {name: "Kerryman Irish Bar & Restaurant", place: {address: "661 N Clark St", city: "Chicago", state: "IL", phone: "123-456-7890", location: [441.894181, -87.631009]}, rating: 4, imgURL: "https://s-media-cache-ak0.pinimg.com/originals/b2/60/0b/b2600b1fa08b5115221dda47fd7db606.jpg"},
-    {name: "Old Town Pour House", place: {address: "1419 N Wells St", city: "Chicago", state: "IL", phone: "312-477-2800", location: [441.894181, -87.631009]}, rating: 4, imgURL: "http://www.nicholashartman.com/_images/logos/l_Pour-House_363636.jpg"},
+    {name: "Old Town Pour House", place: {address: "1419 N Wells St", city: "Chicago", state: "IL", phone: "312-477-2800", location: [441.894181, -87.631009]}, rating: 4, imgURL: "http://www.oldtownpourhouse.com/filebin/images/pourhouse/splash_logo.png"},
     {name: "The VIG", place: {address: "1527 N Wells St", city: "Chicago", state: "IL", phone: "312-982-2186", location: [441.894181, -87.631009]}, rating: 4, imgURL: "https://pbs.twimg.com/profile_images/555553419818061825/JUuXaHUi.jpeg"}
   ]
 };
